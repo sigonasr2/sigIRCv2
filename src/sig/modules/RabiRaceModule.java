fb83cfd8504d0d8ba649e9a9084c9bb4a1255406
sigonasr2 <sigonasr2@gmail.com>
2020-03-19 15:05:18 +0900

    Rabi Race Sync and update processes now do not clash.

--- src/sig/modules/RabiRaceModule.java
+++ src/sig/modules/RabiRaceModule.java
@@ -93,6 +93,7 @@ public class RabiRaceModule extends Module{
 	public HashMap<Integer,Integer> newmapdata = new HashMap<Integer,Integer>();
 	public static ScheduledExecutorService scheduler,scheduler2; 
 	int frames=0;
+	public static boolean regularupdatePerformed=false;
 	
 	public static final int[] RESTRICTED_EVENTS = new int[] {256,257,260,262,264,265,266,267,268,269,271,272,278,279,284,289,290,295,296,307,315,316,322,323,324,331,333,344,345,371,377,379,385,386,387,399,412,427,428,451,452,464,465,484,516,517,518,519};
 	
@@ -188,12 +189,14 @@ public class RabiRaceModule extends Module{
 	private void RunRabiRaceUpdater() {
 		if (foundRabiRibi) {
 			UpdateMyProfile();
+			regularupdatePerformed=true;
 		}
 	}
 
 	private void RunRabiRace() {
 		CheckRabiRibiClient();
-		if (foundRabiRibi) {
+		if (foundRabiRibi && 
+				regularupdatePerformed) {
 			FileUtils.logToFile("["+System.currentTimeMillis()+"]Start update cycle...", "debug.log");
 			myProfile.uploadProfile();
 			getSessionList();
@@ -204,6 +207,7 @@ public class RabiRaceModule extends Module{
 				RequestData("tmp.data","key=keepalivesession&session="+mySession.getID());
 				FileUtils.logToFile("["+System.currentTimeMillis()+"]Requested data"+"key=keepalivesession&session="+mySession.getID(), "debug.log");
 			}
+			regularupdatePerformed=false;
 		}
 	}
 
